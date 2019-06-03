e34f82730eebb8ceec602c545bc4dea948f9a1c1
Rastafactory <34970000+Rastafactory@users.noreply.github.com>
2019-06-03 21:54:40 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('serve', function () {
+	var options = {
+		script: 'app.js',
+		delayTime: 1,
+		env: {
+			'PORT': 3000
+		},
+		watch: jsFiles
+    };
+    console.log('Listening on port: ' + options.env.PORT);
+
+	return nodemon(options)
+		.on('restart', function (ev) {
+            console.log('restarting...');
+            console.log('Listening on port: ' + options.env.PORT);
+        });
+        
+});
