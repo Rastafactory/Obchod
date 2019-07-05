c4d0c4f8d42d0b915621742ed59a157def26d67f
Rastafactory <34970000+Rastafactory@users.noreply.github.com>
2019-07-05 02:17:19 +0200

    lets start to make our business

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   paypal: {
       businessEmail: '',
       url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
-      currency: 'USD'
+      currency: 'EUR'
   },
   secret: 'nanana',
   name: 'nodeStore',
@@ -11,7 +11,7 @@ module.exports = {
       sessions: 'sessions'
   },
   locale: {
-      lang: 'en-US',
-      currency: 'USD'
+      lang: 'en-SK',
+      currency: 'EUR'
   }
 };
--- public/products/js/map-active.js
+++ public/products/js/map-active.js
@@ -1,198 +1,36 @@
+// In this example, we center the map, and add a marker, using a LatLng object
+// literal instead of a google.maps.LatLng object. LatLng object literals are
+// a convenient way to add a LatLng coordinate and, in most cases, can be used
+// in place of a google.maps.LatLng object.
+
 var map;
-var latlng = new google.maps.LatLng(51.5074, 0.1278);
-var stylez = [
-    {
-        "featureType": "administrative.locality",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#2c2e33"
-            },
-            {
-                "saturation": 7
-            },
-            {
-                "lightness": 19
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#ffffff"
-            },
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#ffffff"
-            },
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#bbc0c4"
-            },
-            {
-                "saturation": -93
-            },
-            {
-                "lightness": 31
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "hue": "#bbc0c4"
-            },
-            {
-                "saturation": -93
-            },
-            {
-                "lightness": 31
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "hue": "#bbc0c4"
-            },
-            {
-                "saturation": -93
-            },
-            {
-                "lightness": -2
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#e9ebed"
-            },
-            {
-                "saturation": -90
-            },
-            {
-                "lightness": -8
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#e9ebed"
-            },
-            {
-                "saturation": 10
-            },
-            {
-                "lightness": 69
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#e9ebed"
-            },
-            {
-                "saturation": -78
-            },
-            {
-                "lightness": 67
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    }
-];
-var mapOptions = {
-    zoom: 14,
-    center: latlng,
-    scrollwheel: false,
-    scaleControl: false,
-    disableDefaultUI: true,
-    mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
-    }
-};
-map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-var geocoder_map = new google.maps.Geocoder();
-var address = 'London';
-geocoder_map.geocode({
-    'address': address
-}, function (results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            icon: 'img/core-img/pin.png',
-            position: map.getCenter()
-        });
-    } else {
-        alert("Geocode was not successful for the following reason: " + status);
-    }
-});
-var mapType = new google.maps.StyledMapType(stylez, {
-    name: "Grayscale"
-});
-map.mapTypes.set('gMap', mapType);
-map.setMapTypeId('gMap');
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: {lat: 48.148598, lng: 17.107748}
+  };
+  map = new google.maps.Map(document.getElementById('googleMap'),
+      mapOptions);
+
+  var marker = new google.maps.Marker({
+    // The below line is equivalent to writing:
+    // position: new google.maps.LatLng(-34.397, 150.644)
+    position: {lat: 48.148598, lng: 17.107748},
+    map: map
+  });
+
+  // You can use a LatLng literal in place of a google.maps.LatLng object when
+  // creating the Marker object. Once the Marker object is instantiated, its
+  // position will be available as a google.maps.LatLng object. In this case,
+  // we retrieve the marker's position using the
+  // google.maps.LatLng.getPosition() method.
+  var infowindow = new google.maps.InfoWindow({
+    content: '<p>Marker Location:' + marker.getPosition() + '</p>'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
